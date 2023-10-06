4616cc38f95ede9d1c05b7c8355dd528ad75cd98
yulius10 <feligomez160@gmail.com>
2023-10-05 20:09:09 -0500

    yulius10

--- web-services/main.py
+++ web-services/main.py
@@ -1,4 +1,19 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 import store
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return[1,2,3]
+
+@app.get('/contact',response_class=HTMLResponse)
+def get_list():
+    return """
+        <h1>Hola soy una pagina</h1>
+        <p>soy un parrafo</h1>
+    """
 
 def run():
     store.get_categories()
